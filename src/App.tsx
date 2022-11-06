d6cd1aefe98f547b379d18da8d2bbc220604892d
Vladimir Rutsky <rutsky@google.com>
2022-11-06 10:18:42 +0000

    Use Mantine's Stack and remove more unused styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 import { useState } from 'react'
-import './App.css'
-import { Button, MantineProvider, Text } from '@mantine/core';
+import { Button, MantineProvider, Stack, Text } from '@mantine/core';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
-      <div>
-        <img src="/prufen.svg" className="logo" alt="Prufen logo" />
-      </div>
-      <Text>Prufen</Text>
-      <div className="card">
+      <Stack align="center" mt={50}>
+        <div>
+          <img src="/prufen.svg" className="logo" alt="Prufen logo" />
+        </div>
+        <Text>Prufen</Text>
         <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </Button>
-      </div>
+      </Stack>
     </MantineProvider>
   )
 }
