06b32788b7de753d8da286f28133e51bac3b0ea7
Vladimir Rutsky <rutsky@google.com>
2022-11-06 09:39:18 +0000

    Use Mantine Button + remove unused styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import { Button, MantineProvider, Text } from '@mantine/core';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <MantineProvider withGlobalStyles withNormalizeCSS>
       <div>
         <img src="/prufen.svg" className="logo" alt="Prufen logo" />
       </div>
-      <h1>Prufen</h1>
+      <Text>Prufen</Text>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
       </div>
-    </div>
+    </MantineProvider>
   )
 }
 
