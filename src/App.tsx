9a11b90445c8ed8d993e5e91a48dc81921a38651
Vladimir Rutsky <rutsky@google.com>
2022-11-06 11:27:40 +0000

    Add header and a color scheme switcher
    Based on https://mantine.dev/guides/dark-theme/

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,54 @@
 import { useState } from 'react'
-import { Button, MantineProvider, Stack, Text } from '@mantine/core';
+import { Button, Center, ColorScheme, ColorSchemeProvider, Group, Header, MantineProvider, Stack, Text, ActionIcon, useMantineColorScheme } from '@mantine/core';
+import { IconSun, IconMoonStars } from '@tabler/icons';
+
+function ColorThemeSwitcher() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
+
+  return (
+    <ActionIcon
+      variant="outline"
+      color={dark ? 'yellow' : 'blue'}
+      onClick={() => toggleColorScheme()}
+      title="Toggle color scheme"
+    >
+      {dark ? <IconSun size={18} /> : <IconMoonStars size={18} />}
+    </ActionIcon>
+  );
+}
+
+function PrufenLogo() {
+  return (
+    <Group spacing="xs">
+      <img src="/prufen.svg" className="logo" alt="Prufen logo" />
+      <Text>Prufen</Text>
+    </Group>
+  )
+}
 
 function App() {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
   const [count, setCount] = useState(0)
 
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Stack align="center" mt={50}>
-        <div>
-          <img src="/prufen.svg" className="logo" alt="Prufen logo" />
-        </div>
-        <Text>Prufen</Text>
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-      </Stack>
-    </MantineProvider>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+        <Header height={40}>
+          <Group position="apart">
+            <PrufenLogo />
+            <ColorThemeSwitcher />
+          </Group>
+        </Header>
+        <Stack align="center" mt={50}>
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+        </Stack>
+      </MantineProvider>
+    </ColorSchemeProvider>
   )
 }
 
