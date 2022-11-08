e2a28b112595f73bd293ccd3bffdaf18cb8b8434
Vladimir Rutsky <rutsky@google.com>
2022-11-08 21:13:26 +0000

    Use React Router for the root page + add error page handler
    Following https://reactrouter.com/en/main/start/tutorial

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from './App'
+import ErrorPage from './ErrorPage'
 import './index.css'
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
