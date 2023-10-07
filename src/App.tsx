e26a45a288a69c110f125845ef2b31e1039ea3ba
Vladimir Rutsky <rutsky@google.com>
2023-10-07 21:06:30 +0000

    Update Mantine to most recent version
    Manually update versions in `packages.json`:
    
    ```
    -        "@mantine/core": "^5.7.1",
    -        "@mantine/hooks": "^5.7.1",
    +        "@mantine/core": "^7.1.2",
    +        "@mantine/hooks": "^7.1.2",
    ```
    
    ```
    $ npm install
    ```
    
    See that nothing works and go through the documentation to migrate to never versions:
    
    https://v6.mantine.dev/changelog/6-0-0/
    
    https://mantine.dev/changelog/7-0-0/
    
    Following https://mantine.dev/guides/vite/:
    
    ```
    $ npm install --save-dev postcss postcss-preset-mantine postcss-simple-vars
    ```
    
    Created `postcss.config.js`.
    
    Re-created header using `<header>` tag and an example from https://ui.mantine.dev/category/headers/ and moving the implementation to a separate file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,21 @@
+import '@mantine/core/styles.css';
+
 import { useState } from 'react'
-import { Button, ColorScheme, ColorSchemeProvider, Group, Header, MantineProvider, Stack, Text, ActionIcon, useMantineColorScheme } from '@mantine/core';
-import { IconSun, IconMoonStars } from '@tabler/icons';
-
-function ColorThemeSwitcher() {
-  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
-  const dark = colorScheme === 'dark';
-
-  return (
-    <ActionIcon
-      variant="outline"
-      color={dark ? 'yellow' : 'blue'}
-      onClick={() => toggleColorScheme()}
-      title="Toggle color scheme"
-    >
-      {dark ? <IconSun size={18} /> : <IconMoonStars size={18} />}
-    </ActionIcon>
-  );
-}
-
-function PrufenLogo() {
-  return (
-    <Group spacing="xs">
-      <img src="/prufen.svg" className="logo" alt="Prufen logo" />
-      <Text>Prufen</Text>
-    </Group>
-  )
-}
+import { Button, MantineProvider, Stack } from '@mantine/core';
+import Header from './Header';
 
 function App() {
-  const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
-  const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
   const [count, setCount] = useState(0)
 
   return (
-    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-      <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-        <Header height={40}>
-          <Group position="apart">
-            <PrufenLogo />
-            <ColorThemeSwitcher />
-          </Group>
-        </Header>
-        <Stack align="center" mt={50}>
-          <Button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </Button>
-        </Stack>
-      </MantineProvider>
-    </ColorSchemeProvider>
+    <MantineProvider>
+      <Header />
+      <Stack align="center" mt={50}>
+        <Button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+      </Stack>
+    </MantineProvider>
   )
 }
 
---
+++ src/Header.tsx
@@ -0,0 +1,39 @@
+import { Container, Group, Text, ActionIcon, useMantineColorScheme } from '@mantine/core';
+import { IconSun, IconMoonStars } from '@tabler/icons';
+import classes from './Header.module.css';
+
+function ColorThemeSwitcher() {
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
+
+  return (
+    <ActionIcon
+      variant="outline"
+      color={dark ? 'yellow' : 'blue'}
+      onClick={() => toggleColorScheme()}
+      title="Toggle color scheme"
+    >
+      {dark ? <IconSun size={18} /> : <IconMoonStars size={18} />}
+    </ActionIcon>
+  );
+}
+
+function PrufenLogo() {
+  return (
+    <Group>
+      <img src="/prufen.svg" className="logo" alt="Prufen logo" />
+      <Text>Prufen</Text>
+    </Group>
+  )
+}
+
+export default function Header() {
+  return (
+    <header className={classes.header}>
+      <Container size="md" className={classes.inner}>
+        <PrufenLogo />
+        <ColorThemeSwitcher />
+      </Container>
+    </header>
+  );
+}
