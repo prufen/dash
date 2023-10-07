5bb4c3c78c1b9be59dc0454e3c45ae4e467ccf24
Vladimir Rutsky <rutsky@google.com>
2023-10-07 21:39:29 +0000

    Fix error message handling
    Production build fails:
    
    ```
    $ npm run build
    
    > prufen-dash@0.0.0 build
    > tsc && vite build
    
    src/ErrorPage.tsx:12:13 - error TS18046: 'error' is of type 'unknown'.
    
    12         <i>{error.statusText || error.message}</i>
                   ~~~~~
    
    src/ErrorPage.tsx:12:33 - error TS18046: 'error' is of type 'unknown'.
    
    12         <i>{error.statusText || error.message}</i>
                                       ~~~~~
    
    Found 2 errors in the same file, starting at: src/ErrorPage.tsx:12
    ```
    
    Using this answer: https://stackoverflow.com/a/76126878/391865 with `error.data.message` instead of `error.error?.message` as suggested in comments.

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -1,15 +1,27 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
+  console.error("useRouteError():", error);
+
+  let errorMessage: string;
+  if (isRouteErrorResponse(error)) {
+    // error is type `ErrorResponse`
+    errorMessage = error.data.message || error.statusText;
+  } else if (error instanceof Error) {
+    errorMessage = error.message;
+  } else if (typeof error === "string") {
+    errorMessage = error;
+  } else {
+    errorMessage = "Unknown error";
+  }
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{errorMessage}</i>
       </p>
       <p>
         Try navigating to the <a href="/">start page</a>.
