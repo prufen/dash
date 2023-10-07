837cd4ddfdeaa048c9daa6a9365f63c82f82957e
Vladimir Rutsky <rutsky@google.com>
2023-10-07 22:00:15 +0000

    Drop empty `index.css`
    Will use CSS Modules later if that will be needed.

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import ErrorPage from "./ErrorPage";
-import "./index.css";
 
 // Using HashRouter router as the app will be hosted on GitHub Pages, which
 // doesn't easily support BrowserRouter:
