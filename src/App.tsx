610594b9a35adb4a251a72efd631fb58ce95b7e7
Vladimir Rutsky <rutsky@google.com>
2023-10-08 14:05:20 +0000

    Wrap outlet into Mantine container
    Makes everything centered and nice looking by default.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import "@mantine/core/styles.css";
 
 import { Outlet } from "react-router-dom";
-import { MantineProvider } from "@mantine/core";
+import { Container, MantineProvider } from "@mantine/core";
 import Header from "./Header";
 
 export default function App() {
   return (
     <MantineProvider>
       <Header />
-      <Outlet />
+      <Container>
+        <Outlet />
+      </Container>
     </MantineProvider>
   );
 }
